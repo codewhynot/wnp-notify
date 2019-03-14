c16215baa9c3a653443671205d8bce666e90b20c
codewhynot <codewhynot@gmail.com>
2019-03-14 17:32:01 +0200

    Updated: first publish

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict'
+
 class Notify {
     constructor ({...params} = {}) {
 
@@ -6,20 +8,29 @@ class Notify {
             defaultMessage,
             maxCount,
             customID,
-            debounceDelay,
             showTime,
+            autoDelete,
         } = params;
 
         this.body = document.querySelector('body');
         this.id = customID ? customID : this.randomID(5);
         this.defaultMessage = defaultMessage ? defaultMessage : 'Whynotpack notify! ;)';
         this.customClass = customClass ? customClass : 'default';
-        this.maxCount = maxCount ? maxCount : 5;
-        this.debounceDelay = debounceDelay ? debounceDelay : 0.3;
-        this.showTime = showTime ? showTime : 2;
+        this.maxCount = maxCount ? maxCount : 10;
+        this.showTime = showTime ? showTime : 5;
+        this.autoDelete = autoDelete ? autoDelete : false;
 
         //init wrapper
         this.appendWrapper(this.id);
+
+        // notify wrapper
+        this.area = document.querySelector('#' + this.id);
+
+        //notify array
+        this.notify = [];
+
+        //delete array
+        this.autoDeleteArray = [];
 
     }
     make (tag) {
@@ -51,50 +62,52 @@ class Notify {
 
     createWrapper (id) {
         const wrapper = this.make('div');
-        wrapper.classList.add('notify__wrapper');
+        wrapper.classList.add('wnp-notify__wrapper');
         wrapper.setAttribute('id', id);
         return wrapper;
     }
 
-    debounce (callback, delay) {
-        clearTimeout(this.timeout);
-        this.timeout = setTimeout(() => {
-            callback();
-        }, delay)
-    }
-
-    createElement (type,data) {
-        const notifyType = type === 'e' ? 'error' : type === 's' ? 'success' : 'warning';
-
-        const notifyText = data !== undefined ? data
-            : this.defaultMessage;
+    createElement (type,data,callback) {
+        const notifyType = type === 'e' ? 'error' : type === 's' ? 'success' : 'info';
+        const notifyText = data ? data : this.defaultMessage;
+        const notifyID = this.randomID(3);
 
         //creating elements
         const box = this.make('div');
         const icon = this.make('div');
         const description = this.make('div');
-        const text = this.make('span');
+        const title = this.make('span');
+        const desc = this.make('span');
+        const close = this.make('div');
 
         //adding classes
-        box.classList.add('notify' + this.id);
+        box.classList.add('wnp-notify');
+        box.setAttribute('id',notifyID);
         box.classList.add(this.customClass);
         box.classList.add(notifyType);
-        icon.classList.add('notify__icon');
-        description.classList.add('notify__description');
-        text.classList.add('notify__text');
+        icon.classList.add('wnp-notify__icon');
+        description.classList.add('wnp-notify__description');
+        title.classList.add('wnp-notify__text');
+        desc.classList.add('wnp-notify__desc');
+        close.classList.add('wnp-notify__close');
 
         //appending elements
         box.appendChild(icon);
-        text.textContent = notifyText;
-        description.appendChild(text);
+        box.appendChild(close);
+        title.textContent = notifyText;
+        desc.textContent = 'View details';
+        description.appendChild(title);
+        description.appendChild(desc);
         box.appendChild(description);
-
-        return box;
+        callback(box,notifyID);
     }
 
-    showElement (type, data) {
-        const area = document.querySelector('#' + this.id);
-        area.appendChild(this.createElement(type,data))
+    showElement (id,callback) {
+        this.setVisibility();
+        setTimeout(() => {
+            document.querySelector('#' + id).classList.add('isVisible');
+        },50);
+        callback();
     }
 
     appendWrapper (id) {
@@ -102,16 +115,82 @@ class Notify {
         this.body.appendChild(wrapper);
     }
 
-    getLength () {
-        const items = document.querySelectorAll('.notify' + this.id);
-        return [...items].length;
+    closeNotify (id) {
+        document.querySelector('#' + id + ' .wnp-notify__close').removeEventListener('click', this.closeNotify.bind(this, id));
+        this.notify.forEach((target, index) => {
+            if (target.item.id === id) {
+                clearTimeout(this.autoDeleteArray[index]);
+                this.autoDeleteArray.splice(index, 1);
+                this.deleteElement(this.notify.splice(index, 1)[0]);
+            }
+        });
+        setTimeout(() => {
+            this.setVisibility();
+        }, 300);
+    }
+
+    checkChildren () {
+         return [...this.area.children];
+    }
+
+    addElement (type, data, callback) {
+        this.createElement(type, data, (item, id) => {
+            this.notify.push({item});
+            if (this.checkChildren().length <= 0) {
+                this.area.appendChild(this.notify[this.notify.length - 1].item);
+            } else {
+                this.area.insertBefore(this.notify[this.notify.length - 1].item, this.checkChildren()[0]);
+            }
+            document.querySelector('#' + id + ' .wnp-notify__close').addEventListener('click', this.closeNotify.bind(this, id));
+            callback(id);
+        });
+
+    }
+    setVisibility () {
+        if (this.notify.length > 0) {
+            this.area.classList.add('isVisible');
+        } else {
+            this.area.classList.remove('isVisible');
+        }
+    }
+    deleteElement (data) {
+        if (this.autoDelete && !data) {
+            this.autoDeleteArray.push(setTimeout(() => {
+                if (this.notify.length > 0) {
+                    const target = this.notify.shift();
+                    target.item.classList.remove('isVisible');
+                    setTimeout(() => {
+                        this.area.removeChild(target.item);
+                    }, 350);
+                } else {
+                    return false;
+                }
+            }, this.showTime * 1000));
+        } else if (this.autoDelete && data) {
+            data.item.classList.remove('isVisible');
+            setTimeout(() => {
+                this.area.removeChild(data.item);
+            }, 350);
+        } else if (!this.autoDelete && data) {
+            data.item.classList.remove('isVisible');
+            setTimeout(() => {
+                this.area.removeChild(data.item);
+            }, 350);
+        } else {
+            return false;
+        }
+        setTimeout(() => {
+            this.setVisibility();
+        }, 300);
     }
 
     message (type, data) {
-        if (this.getLength() >= this.maxCount) return false;
-        this.debounce(() => {
-            this.showElement(type,data);
-        }, this.debounceDelay * 1000);
+        if (this.notify.length >= this.maxCount) return false;
+        this.addElement(type,data, id => {
+            this.showElement(id,() => {
+                this.deleteElement();
+            })
+        });
     }
 }
 
